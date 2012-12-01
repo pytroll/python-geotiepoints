cfa3e0878ba503304e266036bbdcbcc0c44f9346
Adam Dybbroe <adam.dybbroe@smhi.se>
2012-12-01 13:10:12 +0100

    Mockup to avoid import errors when using autodoc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,6 +21,30 @@ sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../geotiepoints'))
 
 
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['numpy', 'scipy', 'pyresample']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
