6bc652a51d720de2725a693e1faf3e2a9107a022
Sauli Joro <sauli.joro@eumetsat.int>
2020-05-25 10:49:56 +0000

    Incorporate reviewer comments. Use of numpy instead of dask where possible. Code clean-up.

--- geotiepoints/viiinterpolator.py
+++ geotiepoints/viiinterpolator.py
@@ -22,24 +22,20 @@
 
 """Interpolation of geographical tiepoints for the VII products.
 It follows the description provided in document "EPS-SG VII Level 1B Product Format Specification".
+The interpolation functions are implemented for xarray.DataArrays as input.
 """
 
 import xarray as xr
 import dask.array as da
-from itertools import chain
+import numpy as np
 
 # MEAN EARTH RADIUS AS DEFINED BY IUGG
 MEAN_EARTH_RADIUS = 6371008.7714  # [m]
 
-# DEFAULT THRESHOLDS FOR CARTESIAN COORDINATES INTERPOLATION
-# THR_CARTESIAN: latitude limit above which interpolation is performed in cartesian coordinates
-THR_CARTESIAN = 60.
-# THR_USE_XY: z-coordinate limit above which latitude is computed from x and y coordinates
-THR_USE_XY = 0.8
-
 
 def tie_points_interpolation(data_on_tie_points, scan_alt_tie_points, tie_points_factor):
     """Interpolate the data from the tie points to the pixel points.
+
     The data are provided as a list of xarray DataArray objects, allowing to interpolate on several arrays
     at the same time; however the individual arrays must have exactly the same dimensions.
 
@@ -75,11 +71,11 @@ def tie_points_interpolation(data_on_tie_points, scan_alt_tie_points, tie_points
     # Create the grids used for the interpolation along the track (must not include the spurious points between scans)
     tie_grid_alt = da.arange(0, n_pixel_alt + 1, tie_points_factor)
     n_pixel_alt_per_scan = (scan_alt_tie_points - 1) * tie_points_factor
-    pixel_grid_alt = iter(())
+    pixel_grid_alt = []
     for j_scan in range(n_scans):
         start_index_scan = j_scan * scan_alt_tie_points * tie_points_factor
-        pixel_grid_alt = chain(pixel_grid_alt, range(start_index_scan, start_index_scan + n_pixel_alt_per_scan))
-    pixel_grid_alt = list(pixel_grid_alt)
+        pixel_grid_alt.append(da.arange(start_index_scan, start_index_scan + n_pixel_alt_per_scan))
+    pixel_grid_alt = da.concatenate(pixel_grid_alt)
 
     # Loop on all arrays
     data_on_pixel_points = []
@@ -101,9 +97,10 @@ def tie_points_interpolation(data_on_tie_points, scan_alt_tie_points, tie_points
 
 def tie_points_geo_interpolation(longitude, latitude,
                                  scan_alt_tie_points, tie_points_factor,
-                                 thr_cartesian=THR_CARTESIAN,
-                                 thr_use_xy=THR_USE_XY):
+                                 lat_threshold_use_cartesian=60.,
+                                 z_threshold_use_xy=0.8):
     """Interpolate the geographical position from the tie points to the pixel points.
+
     The longitude and latitude values are provided as xarray DataArray objects.
 
     Args:
@@ -119,8 +116,8 @@ def tie_points_geo_interpolation(longitude, latitude,
         latitude: xarray DataArray containing the latitude values defined on the tie points (degrees).
         scan_alt_tie_points: number of tie points along the satellite track for each scan.
         tie_points_factor: sub-sampling factor of tie points wrt pixel points.
-        thr_cartesian: latitude threshold to use cartesian coordinates.
-        thr_use_xy: z threshold to compute latitude from x and y in cartesian coordinates.
+        lat_threshold_use_cartesian: latitude threshold to use cartesian coordinates.
+        z_threshold_use_xy: z threshold to compute latitude from x and y in cartesian coordinates.
 
     Returns:
         two xarray DataArray objects containing the interpolated longitude and latitude values on the pixel points.
@@ -131,7 +128,7 @@ def tie_points_geo_interpolation(longitude, latitude,
         raise ValueError("The dimensions of longitude and latitude don't match")
 
     # Determine if the interpolation should be done in cartesian or geodetic coordinates
-    to_cart = da.max(da.fabs(latitude)) > thr_cartesian or (da.max(longitude) - da.min(longitude)) > 180.
+    to_cart = np.max(np.fabs(latitude)) > lat_threshold_use_cartesian or (np.max(longitude) - np.min(longitude)) > 180.
 
     if to_cart:
 
@@ -141,7 +138,7 @@ def tie_points_geo_interpolation(longitude, latitude,
                                                                 scan_alt_tie_points,
                                                                 tie_points_factor)
 
-        interp_longitude, interp_latitude = _xyz2lonlat(interp_x, interp_y, interp_z, thr_use_xy)
+        interp_longitude, interp_latitude = _xyz2lonlat(interp_x, interp_y, interp_z, z_threshold_use_xy)
 
     else:
 
@@ -163,34 +160,34 @@ def _lonlat2xyz(lons, lats):
         tuple of arrays containing the x, y, and z values in meters.
 
     """
-    lons_rad = da.deg2rad(lons)
-    lats_rad = da.deg2rad(lats)
-    x_coords = MEAN_EARTH_RADIUS * da.cos(lats_rad) * da.cos(lons_rad)
-    y_coords = MEAN_EARTH_RADIUS * da.cos(lats_rad) * da.sin(lons_rad)
-    z_coords = MEAN_EARTH_RADIUS * da.sin(lats_rad)
+    lons_rad = np.deg2rad(lons)
+    lats_rad = np.deg2rad(lats)
+    x_coords = MEAN_EARTH_RADIUS * np.cos(lats_rad) * np.cos(lons_rad)
+    y_coords = MEAN_EARTH_RADIUS * np.cos(lats_rad) * np.sin(lons_rad)
+    z_coords = MEAN_EARTH_RADIUS * np.sin(lats_rad)
     return x_coords, y_coords, z_coords
 
 
-def _xyz2lonlat(x_coords, y_coords, z_coords, thr_use_xy):
+def _xyz2lonlat(x_coords, y_coords, z_coords, z_threshold_use_xy=0.8):
     """Get longitudes and latitudes from cartesian coordinates.
 
     Args:
         x_coords: array containing the x values in meters.
         y_coords: array containing the y values in meters.
         z_coords: array containing the z values in meters.
-        thr_use_xy: z threshold to compute latitude from x and y in cartesian coordinates.
+        z_threshold_use_xy: z threshold to compute latitude from x and y in cartesian coordinates.
 
     Returns:
         tuple of arrays containing the longitude and latitude values in degrees.
 
     """
-    r = da.sqrt(x_coords ** 2 + y_coords ** 2)
-    thr_z = thr_use_xy * MEAN_EARTH_RADIUS
-    lons = da.rad2deg(da.arccos(x_coords / r)) * da.sign(y_coords)
+    r = np.sqrt(x_coords ** 2 + y_coords ** 2)
+    thr_z = z_threshold_use_xy * MEAN_EARTH_RADIUS
+    lons = np.rad2deg(np.arccos(x_coords / r)) * np.sign(y_coords)
     # Compute latitude from z at low z and from x and y at high z
     lats = xr.where(
-        da.logical_and(da.less(z_coords, thr_z), da.greater(z_coords, -thr_z)),
-        90. - da.rad2deg(da.arccos(z_coords / MEAN_EARTH_RADIUS)),
-        da.sign(z_coords) * (90. - da.rad2deg(da.arcsin(r / MEAN_EARTH_RADIUS)))
+        np.logical_and(np.less(z_coords, thr_z), np.greater(z_coords, -thr_z)),
+        90. - np.rad2deg(np.arccos(z_coords / MEAN_EARTH_RADIUS)),
+        np.sign(z_coords) * (90. - np.rad2deg(np.arcsin(r / MEAN_EARTH_RADIUS)))
     )
     return lons, lats
