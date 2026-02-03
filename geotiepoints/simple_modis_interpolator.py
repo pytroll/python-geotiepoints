3ccdf6fe4664d76b7e5d706fa5ea2d5f8af34c48
David Hoese <david.hoese@ssec.wisc.edu>
2026-02-02 20:54:14 -0600

    Relicense to Apache Version 2.0

--- geotiepoints/simple_modis_interpolator.py
+++ geotiepoints/simple_modis_interpolator.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# Copyright (c) 2021 Python-geotiepoints developers
-#
-# This file is part of python-geotiepoints.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Interpolate MODIS 1km navigation arrays to 250m and 500m resolutions.
 
 The code used here is a rewrite of the IDL function ``MODIS_GEO_INTERP_250``
--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-# Copyright (c) 2012-2023 Python-geotiepoints developers
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Setting up the geo_interpolator project."""
 
 import sys
@@ -106,7 +92,7 @@ if __name__ == "__main__":
               "Topic :: Scientific/Engineering",
               "Programming Language :: Python :: Free Threading :: 1 - Unstable",
           ],
-          license="GPL-3.0-or-later",
+          license="Apache-2.0",
           license_files=["LICENSE.txt"],
           url="https://github.com/pytroll/python-geotiepoints",
           packages=find_packages(),
